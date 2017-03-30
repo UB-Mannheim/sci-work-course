d1e2449073f6256788dfb5a0f7bef8ab02c78e58
Philipp Zumstein <zuphilip@gmail.com>
2017-03-30 07:17:08 +0200

    Update RESET_COMMIT in reset-birth-dates.sh

--- reset-birth-dates.sh
+++ reset-birth-dates.sh
@@ -3,7 +3,7 @@
 if [[ -n "$1" ]]; then
     RESET_COMMIT=$1
 else
-    RESET_COMMIT="c4f307d6620ae5543615453379d17b033b59ecdf"
+    RESET_COMMIT="7cf80935c01bb2b15e8164765025e1bd86d0dd6c"
 fi
 set -x
 git checkout "$RESET_COMMIT" data/birth-dates/*
