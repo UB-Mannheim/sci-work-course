d6053710b91374aa6dc8d7df4f9dd2b583402544
Philipp Zumstein <zuphilip@gmail.com>
2018-01-11 18:02:33 +0100

    Extend reset-birth-dates.sh

--- reset-birth-dates.sh
+++ reset-birth-dates.sh
@@ -6,6 +6,7 @@ else
     RESET_COMMIT="f2cfe46245ef67f35302e79b0d9c4993011d082c"
 fi
 set -x
+git pull origin
 git checkout "$RESET_COMMIT" data/birth-dates/*
 git commit -m "Reset birth dates"
 set +x
